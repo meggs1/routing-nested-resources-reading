0f9fc93b2f98552660ca46fc01af318d4ac83483
megan <megan.schertz@gmail.com>
2021-03-22 19:01:45 -0500

    commit tree 05d529c5a22e7c5952d75ee7bf67125d4c4d151a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,28 @@
 class AuthorsController < ApplicationController
 
   def show
+    # @author = Author.find(params[:id])
+    @post = Post.find(params[:id])
+  end
+
+  def posts_index
+    # @author = Author.find(params[:id])
+    # @posts = @author.posts
+    # render template: 'posts/index'
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
+  end
+
+  def post
     @author = Author.find(params[:id])
+
+    # Note that because ids are unique by table we can go directly to
+    # Post.find — no need for @author.posts.find...
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
   end
 
 end
